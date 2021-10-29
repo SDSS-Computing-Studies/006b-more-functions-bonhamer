6ec60538fa32709a0d5d51d854ab89ed3f606984
bonhamer <bonh248@deltalearns.ca>
2021-10-29 14:17:01 -0700

    1

--- problem2.py
+++ problem2.py
@@ -6,13 +6,11 @@ Create a function that determines if a triangle is scalene, right or obtuse.
 float: one side  
 float: another side  
 float: 3rd side  
-
 return:
 0 : triangle does not exist
 1 : if the triangle is scalene
 2 : if the triangle is right
 3 : if the triangle is obtuse
-
 Sample assertions:  
 assert triangle(12,5,13) == 2     
 assert triangle(5,3,3) == 1  
@@ -20,3 +18,18 @@ assert triangle(5,15,12) == 3
 assert triangle(1,1,4) == 0  
 (2 points)
 """
+import math
+def triangle(a,b,c):
+    list1 = [a,b,c]
+    list1.sort()
+    a = list1[0]
+    b = list1[1]
+    c = list1[2]
+    if c > a+b:
+        return 0
+    elif c < math.sqrt(a**2+b**2):
+        return 1
+    elif c == math.sqrt(a**2+b**2):
+        return 2
+    elif c > math.sqrt(a**2+b**2):
+        return 3
--- task4.py
+++ task4.py
@@ -4,15 +4,15 @@
 Create a function that determines the area of a circle if given the radius
 1 input parameter
 float: radius
-
 return: float area for the circle
-
 note: Area of a circle is given by A = pi*(square of the radius)
 You may want to use the math module to complete this problem
 """
+import math
 
-def area():
-    return
+def area(a):
+    f = math.pi * (a**2)
+    return f
 
-
+print(round(area(2),2))
 assert round(area(2),2) == 12.57
